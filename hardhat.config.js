cecfb3ff7dc8837c3301313e63fb532589d27b50
cooker0910 <cooker0910@gmail.com>
2022-09-30 01:18:19 -0700

    delete

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,9 +6,10 @@ require("@nomiclabs/hardhat-etherscan");
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
-    goerli: {
-      url: 'https://eth-goerli.g.alchemy.com/v2/cZNwSuFgH1Vlgqnt73U2rzdi9L41P7Qb',
-      accounts: ['']
+    hardhat: {
+      forking: {
+        url: " https://eth-mainnet.alchemyapi.io.v2/cZNwSuFgH1Vlgqnt73U2rzdi9L41P7Qb"
+      }
     }
   },
   solidity: {
